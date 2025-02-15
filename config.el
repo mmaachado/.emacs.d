8533127c5640e143135955e34a36afd6523267eb
Marcelo Machado <mjoelmachadojr@gmail.com>
2025-02-15 00:30:07 -0300

    started adding packages

--- config.el
+++ config.el
@@ -37,3 +37,10 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+(package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package rainbow-mode)
