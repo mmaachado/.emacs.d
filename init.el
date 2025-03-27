bf1f9145a5405e03261d675b44d1a3cba7aa5a4b
Marcelo Machado <mjoelmachadojr@gmail.com>
2025-03-27 14:30:38 -0300

    added nano

--- init.el
+++ init.el
@@ -1,4 +1,68 @@
-(require 'black)
-(require 'sanity)
-(require 'black-emacs)
-(require 'Splash.org)
+(add-to-list 'load-path "~/.emacs.d/nano")
+
+(require 'nano)
+(setq nano-font-family-monospaced "JetBrainsMono")
+(nano-toggle-theme)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-unset-key (kbd "C-z"))
+(delete-selection-mode t)
+
+(setq backup-directory-alist '((".*" . "~/.local/share/Trash/files")))
+
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror 'nomessage)
+
+
+(push '(fullscreen . maximized) initial-frame-alist)
+(push '(fullscreen . maximized) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(column-number-mode 1)
+(save-place-mode 1)
+(recentf-mode 1)
+(savehist-mode 1)
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+(package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package elpy
+  :ensure t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-show-numbers t)
+  ;; To prevent default down-casing.
+  ;; https://emacs.stackexchange.com/questions/10837/how-to-make-company-mode-be-case-sensitive-on-plain-text
+  (setq company-dabbrev-downcase nil)
+  ;; 2023-01-13 From a Reddit post on mixed case issue.
+  (setq company-dabbrev-ignore-case nil)
+  (setq company-dabbrev-code-ignore-case nil))
+
+;; Use `company' everywhere.
+(add-hook 'after-init-hook 'global-company-mode)
